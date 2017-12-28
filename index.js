b55323f82fe157d9e190748e28bc1c8367920945
kevin le <17786629@student.westernsydney.edu.au>
2017-12-28 13:13:15 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(hello) {
+
+  return "I can't hear you!"
+}
+
+function sayHiToGrandma(HELLO) {
+
+  return "YES INDEED!"
+}
